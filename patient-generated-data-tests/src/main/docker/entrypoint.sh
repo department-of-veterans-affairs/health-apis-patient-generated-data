f3c78a35f5b1f6f1a3e3f8e0cf39869847a08cae
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-10-14 16:28:18 -0400

    .

--- patient-generated-data-tests/src/main/docker/entrypoint.sh
+++ patient-generated-data-tests/src/main/docker/entrypoint.sh
@@ -7,14 +7,12 @@ cd $SENTINEL_BASE_DIR
 
 test -n "${K8S_ENVIRONMENT}"
 if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV=$K8S_ENVIRONMENT; fi
-if [ -z "${PATIENT_GENERATED_DATA_URL:-}" ]; then PATIENT_GENERATED_DATA_URL=https://$K8S_LOAD_BALANCER; fi
 
 java-tests \
   --module-name "patient-generated-data-tests" \
   --regression-test-pattern ".*IT\$" \
   --smoke-test-pattern ".*Smoke.*IT\$" \
   -Dsentinel="$SENTINEL_ENV" \
-  -Dpatient-generated-data.url="$PATIENT_GENERATED_DATA_URL" \
   $@
 
 exit $?
--- patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/SmokeTestIT.java
+++ patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/SmokeTestIT.java
@@ -1,7 +1,8 @@
 package gov.va.api.health.patientgenerateddata.tests;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static gov.va.api.health.patientgenerateddata.tests.SystemDefinitions.systemDefinition;
 
+import gov.va.api.health.patientgenerateddata.tests.SystemDefinitions.Service;
 import gov.va.api.health.sentinel.ExpectedResponse;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
@@ -12,12 +13,15 @@ import org.junit.jupiter.api.Test;
 public class SmokeTestIT {
   @Test
   void health() {
-    String url = System.getProperty("patient-generated-data.url");
-    assertThat(url).isNotNull();
-    String request = url + "/actuator/health";
+    Service svc = systemDefinition().internal();
+    String request = svc.url() + "/actuator/health";
     log.info(request);
     ExpectedResponse.of(
-            RestAssured.given().baseUri(url).relaxedHTTPSValidation().request(Method.GET, request))
+            RestAssured.given()
+                .baseUri(svc.url())
+                .port(svc.port())
+                .relaxedHTTPSValidation()
+                .request(Method.GET, request))
         .expect(200);
   }
 }
