9f74a4878273838925ee079af483001a7f67f84c
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-10-14 15:46:16 -0400

    .

--- patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/SmokeTestIT.java
+++ patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/SmokeTestIT.java
@@ -1,5 +1,10 @@
 package gov.va.api.health.patientgenerateddata.tests;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import gov.va.api.health.sentinel.ExpectedResponse;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -8,12 +13,11 @@ public class SmokeTestIT {
   @Test
   void health() {
     String url = System.getProperty("patient-generated-data.url");
-	assertThat(url).isNotNull();
+    assertThat(url).isNotNull();
+    String request = url + "/actuator/health";
+    log.info(request);
     ExpectedResponse.of(
-            RestAssured.given()
-                .baseUri(url)
-                .relaxedHTTPSValidation()
-                .request(Method.GET, url + "/actuator/health"))
+            RestAssured.given().baseUri(url).relaxedHTTPSValidation().request(Method.GET, request))
         .expect(200);
   }
 }
--- patient-generated-data/README.md
+++ patient-generated-data/README.md
@@ -1,1 +1,7 @@
 # patient-generated-data
+
+Main application.
+
+## Local Development
+
+`java -Dspring.profiles.active=dev -jar target/patient-generated-data-${VERSION}.jar`
