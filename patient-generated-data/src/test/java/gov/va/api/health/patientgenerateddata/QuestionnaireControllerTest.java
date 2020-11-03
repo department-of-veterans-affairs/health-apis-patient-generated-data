2c78a33c8a5bfc0717ceb546f7e82cfb1bd85acc
jblefkove-va <46896876+jblefkove-va@users.noreply.github.com>
2020-11-03 09:49:57 -0500

    API-3362 implement questionnaire update (#6)

--- patient-generated-data/src/test/java/gov/va/api/health/patientgenerateddata/QuestionnaireControllerTest.java
+++ patient-generated-data/src/test/java/gov/va/api/health/patientgenerateddata/QuestionnaireControllerTest.java
@@ -3,6 +3,8 @@ package gov.va.api.health.patientgenerateddata;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import gov.va.api.health.autoconfig.configuration.JacksonConfig;
@@ -10,6 +12,9 @@ import gov.va.api.health.r4.api.resources.Questionnaire;
 import java.util.Optional;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.DataBinder;
 
 public class QuestionnaireControllerTest {
   @Test
@@ -17,6 +22,12 @@ public class QuestionnaireControllerTest {
     assertThrows(
         Exceptions.InvalidPayload.class,
         () -> QuestionnaireController.deserializedPayload("x", "notjson", Questionnaire.class));
+  }
+
+  @Test
+  void initDirectFieldAccess() {
+    new QuestionnaireController(mock(QuestionnaireRepository.class))
+        .initDirectFieldAccess(mock(DataBinder.class));
   }
 
   @Test
@@ -36,5 +47,29 @@ public class QuestionnaireControllerTest {
     QuestionnaireRepository repo = mock(QuestionnaireRepository.class);
     assertThrows(
         Exceptions.NotFound.class, () -> new QuestionnaireController(repo).read("notfound"));
+  }
+
+  @Test
+  @SneakyThrows
+  void update_existing() {
+    QuestionnaireRepository repo = mock(QuestionnaireRepository.class);
+    Questionnaire questionnaire = Questionnaire.builder().id("x").build();
+    String payload = JacksonConfig.createMapper().writeValueAsString(questionnaire);
+    when(repo.findById("x"))
+        .thenReturn(Optional.of(QuestionnaireEntity.builder().id("x").payload(payload).build()));
+    assertThat(new QuestionnaireController(repo).update("x", questionnaire))
+        .isEqualTo(ResponseEntity.ok().build());
+    verify(repo, times(1)).save(QuestionnaireEntity.builder().id("x").payload(payload).build());
+  }
+
+  @Test
+  @SneakyThrows
+  void update_new() {
+    QuestionnaireRepository repo = mock(QuestionnaireRepository.class);
+    Questionnaire questionnaire = Questionnaire.builder().id("x").build();
+    String payload = JacksonConfig.createMapper().writeValueAsString(questionnaire);
+    assertThat(new QuestionnaireController(repo).update("x", questionnaire))
+        .isEqualTo(ResponseEntity.status(HttpStatus.CREATED).build());
+    verify(repo, times(1)).save(QuestionnaireEntity.builder().id("x").payload(payload).build());
   }
 }
