09d26a9702904d6f507cfb6d949cc7c4decd01b5
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-11-25 17:09:12 -0500

    .

--- patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/OpenApi.java
+++ patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/OpenApi.java
@@ -3,10 +3,12 @@ package gov.va.api.health.patientgenerateddata;
 import gov.va.api.health.patientgenerateddata.patient.PatientApi;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import javax.ws.rs.Path;
 
 @OpenAPIDefinition(
+    info = @Info(title = "US Core R4", version = "v1"),
     servers = {
       @Server(url = "https://blue.lab.lighthouse.va.gov/pgd/v0/r4/", description = "Sandbox")
     },
