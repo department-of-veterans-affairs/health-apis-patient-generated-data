ed6254c6d1df9a184f5946116663e9fa5873719c
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-11-25 17:10:17 -0500

    .

--- patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/OpenApiIT.java
+++ patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/OpenApiIT.java
@@ -5,20 +5,6 @@ import static gov.va.api.health.patientgenerateddata.tests.RequestUtils.makeRequ
 import org.junit.jupiter.api.Test;
 
 public class OpenApiIT {
-  // value = {"", ""},
-  //  @Test
-  //  void openApi() {
-  //    Service svc = systemDefinition().r4();
-  //    String request = svc.urlWithApiPath() + "actuator/health";
-  //    ExpectedResponse.of(
-  //            RestAssured.given()
-  //                .baseUri(svc.url())
-  //                .port(svc.port())
-  //                .relaxedHTTPSValidation()
-  //                .request(Method.GET, request))
-  //        .expect(200);
-  //  }
-
   @Test
   void openApi() {
     makeRequest(null, "", 200);
