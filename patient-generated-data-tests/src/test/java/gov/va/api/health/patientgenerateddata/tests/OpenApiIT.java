fe1f31ec1f29fe8ff5435c6a4b74387db55ab1a4
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-11-30 16:32:13 -0500

    .

--- patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/OpenApiIT.java
+++ patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/OpenApiIT.java
@@ -1,12 +1,22 @@
 package gov.va.api.health.patientgenerateddata.tests;
 
 import static gov.va.api.health.patientgenerateddata.tests.RequestUtils.makeRequest;
+import static gov.va.api.health.sentinel.EnvironmentAssumptions.assumeEnvironmentIn;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import gov.va.api.health.sentinel.Environment;
+
 public class OpenApiIT {
+  @BeforeAll
+  static void assumeEnvironment() {
+    assumeEnvironmentIn(Environment.LOCAL, Environment.QA);
+  }
+
   @Test
   void openApi() {
+    makeRequest("application/json", "openapi.json", 200);
     makeRequest(null, "openapi.json", 200);
   }
 }
