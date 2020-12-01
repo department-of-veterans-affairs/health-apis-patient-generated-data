4a9dfd3b04a5540ed8cb07da365bd514f6eae821
Adrian Garcia <adrian.garcia@libertyits.com>
2020-12-01 15:57:49 -0500

    Update method added to observationApi

--- patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/observation/ObservationApi.java
+++ patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/observation/ObservationApi.java
@@ -8,8 +8,11 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import javax.validation.Valid;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface ObservationApi {
   @Operation(
@@ -45,7 +48,43 @@ public interface ObservationApi {
               name = "id",
               required = true,
               description =
-                  "The logical id of the resource."
-                      + " Once assigned, this value never changes.")
+                  "The logical id of the resource." + " Once assigned, this value never changes.")
           String id);
+
+  @Operation(
+      summary = "Observation Update",
+      description = "https://www.hl7.org/fhir/r4/observation.html",
+      tags = {"Observation"})
+  @GET
+  @Path("Observation/{id}")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = ResponseEntity.class)))
+  @ApiResponse(
+      responseCode = "400",
+      description = "Bad request",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  @ApiResponse(
+      responseCode = "404",
+      description = "Not found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  ResponseEntity<Void> observationUpdate(
+      @Parameter(
+              in = ParameterIn.PATH,
+              name = "id",
+              required = true,
+              description =
+                  "The logical id of the resource." + " Once assigned, this value never changes.")
+          String id,
+      @Valid @RequestBody Observation observation);
 }
