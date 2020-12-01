5917bb5157e5aae8ac286cdb38c81e1df562d3f0
jblefkove-va <46896876+jblefkove-va@users.noreply.github.com>
2020-12-01 17:44:15 -0500

    API-3881 disable OpenApiIT in lab (#28)

--- patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/OpenApiIT.java
+++ patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/OpenApiIT.java
@@ -1,10 +1,20 @@
 package gov.va.api.health.patientgenerateddata.tests;
 
 import static gov.va.api.health.patientgenerateddata.tests.RequestUtils.makeRequest;
+import static gov.va.api.health.sentinel.EnvironmentAssumptions.assumeEnvironmentIn;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import gov.va.api.health.sentinel.Environment;
+
 public class OpenApiIT {
+  @BeforeAll
+  static void assumeEnvironment() {
+    assumeEnvironmentIn(
+        Environment.LOCAL, Environment.QA, Environment.STAGING, Environment.STAGING_LAB);
+  }
+
   @Test
   void openApi_json() {
     makeRequest("application/json", "openapi.json", 200);
