05109431f8eb53376d9d84295c8ac8750d548bed
Alberto Peirats <72031660+apeirats-va@users.noreply.github.com>
2020-12-09 12:08:56 -0500

    swagger and metadata

--- patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/questionnaireresponse/QuestionnaireResponseApi.java
+++ patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/questionnaireresponse/QuestionnaireResponseApi.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -49,6 +50,63 @@ public interface QuestionnaireResponseApi {
               description =
                   "The logical id of the resource. Once assigned, this value never changes.")
           String id);
+
+  @Operation(
+      summary = "QuestionnaireResponse Search",
+      description = "https://www.hl7.org/fhir/R4/questionnaireresponse.html",
+      tags = {"QuestionnaireResponse"})
+  @GET
+  @Path("QuestionnaireResponse")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              schema = @Schema(implementation = QuestionnaireResponse.Bundle.class),
+              mediaType = "application/fhir+json"))
+  @ApiResponse(
+      responseCode = "400",
+      description = "Bad request",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  @ApiResponse(
+      responseCode = "404",
+      description = "Not found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  QuestionnaireResponse.Bundle questionnaireResponseSearch(
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "_id",
+              required = true,
+              description =
+                  "The logical id of the resource. Once assigned, this value never changes.")
+          String id,
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "authored",
+              description =
+                  "A date or range of dates (maximum of 2) that describes "
+                      + "the date that the response was recorded.")
+          String[] authored,
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "page",
+              description = "The page number of the search result.")
+          @DefaultValue("1")
+          int page,
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "_count",
+              description =
+                  "The number of resources that should be returned in a single page. "
+                      + "The maximum count size is 100.")
+          @DefaultValue("15")
+          int count);
 
   @Operation(
       summary = "QuestionnaireResponse Update",
