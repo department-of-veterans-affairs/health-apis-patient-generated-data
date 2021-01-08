0b85419ecdfa2e1e3704d6473f230a27878f1e00
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-01-08 10:04:52 -0500

    .

--- patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/QuestionnaireIT.java
+++ patient-generated-data-tests/src/test/java/gov/va/api/health/patientgenerateddata/tests/QuestionnaireIT.java
@@ -4,7 +4,9 @@ import static gov.va.api.health.patientgenerateddata.tests.RequestUtils.doGet;
 import static gov.va.api.health.patientgenerateddata.tests.SystemDefinitions.systemDefinition;
 import static gov.va.api.health.sentinel.EnvironmentAssumptions.assumeEnvironmentIn;
 
+import gov.va.api.health.r4.api.resources.Questionnaire;
 import gov.va.api.health.sentinel.Environment;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +32,34 @@ public class QuestionnaireIT {
   @Test
   void read_notFound() {
     doGet("application/json", "Questionnaire/5555555", 404);
+  }
+
+  @Test
+  void search_id() {
+    assumeEnvironmentIn(
+        Environment.LOCAL,
+        Environment.QA,
+        Environment.STAGING,
+        Environment.STAGING_LAB,
+        Environment.LAB);
+    var id = systemDefinition().ids().questionnaire();
+    var response = doGet("application/json", "Questionnaire?_id=" + id, 200);
+    response.expectValid(Questionnaire.Bundle.class);
+  }
+
+  @Test
+  void search_subject() {
+    assumeEnvironmentIn(Environment.LOCAL);
+    // Environment.QA, Environment.STAGING, Environment.STAGING_LAB Environment.LAB
+    String arg = "venue$https://staff.apps.va.gov/VistaEmrService/clinics|534/12975";
+    String query = String.format("?context-type-value=%s", arg);
+    var response = doGet("application/json", "QuestionnaireResponse" + query, 200);
+    Questionnaire.Bundle bundle = response.expectValid(Questionnaire.Bundle.class);
+    assertThat(bundle.entry()).hasSizeGreaterThan(0);
+
+    //    query = String.format("?subject=%s", "unknown");
+    //    response = doGet("application/json", "QuestionnaireResponse" + query, 200);
+    //    bundle = response.expectValid(Questionnaire.Bundle.class);
+    //    assertThat(bundle.entry()).isEmpty();
   }
 }
