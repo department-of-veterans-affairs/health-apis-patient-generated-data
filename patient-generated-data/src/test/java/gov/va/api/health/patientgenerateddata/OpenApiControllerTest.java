0f675487695b1a0f6da55de7a047e64efac767ba
Adrian Garcia <adrian.garcia@libertyits.com>
2021-01-19 09:39:20 -0500

    Added dummy test for OpenApiController to boost coverage

---
+++ patient-generated-data/src/test/java/gov/va/api/health/patientgenerateddata/OpenApiControllerTest.java
@@ -0,0 +1,14 @@
+package gov.va.api.health.patientgenerateddata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class OpenApiControllerTest {
+
+  @Test
+  void openApiCreator() {
+    OpenApiController oac = new OpenApiController();
+    assertThat(oac.openApi()).isNotNull();
+  }
+}
