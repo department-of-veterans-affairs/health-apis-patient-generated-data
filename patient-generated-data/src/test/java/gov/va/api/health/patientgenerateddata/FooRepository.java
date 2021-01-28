1f01727cbf98903a440e2e81ac9a30f876f737a5
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-01-27 19:08:58 -0500

    .

--- patient-generated-data/src/test/java/gov/va/api/health/patientgenerateddata/FooRepository.java
+++ patient-generated-data/src/test/java/gov/va/api/health/patientgenerateddata/FooRepository.java
@@ -3,7 +3,5 @@ package gov.va.api.health.patientgenerateddata;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-// @Loggable
-// @Transactional(isolation = Isolation.READ_UNCOMMITTED)
 public interface FooRepository
     extends CrudRepository<FooEntity, String>, JpaSpecificationExecutor<FooEntity> {}
