433135d63da68af2b51f7c4dad5b7824e2161f3e
Adrian Garcia <adrian.garcia@libertyits.com>
2021-04-09 16:50:35 -0400

    Refactored constructor to allow for more thorough state checking, without the havingValue annotation.

--- patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/SandboxDeleteController.java
+++ patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/SandboxDeleteController.java
@@ -1,10 +1,12 @@
 package gov.va.api.health.patientgenerateddata;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import gov.va.api.health.patientgenerateddata.observation.ObservationRepository;
 import gov.va.api.health.patientgenerateddata.questionnaire.QuestionnaireRepository;
 import gov.va.api.health.patientgenerateddata.questionnaireresponse.QuestionnaireResponseRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,14 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(
     value = "/sandbox-data/r4",
     produces = {"application/json", "application/fhir+json"})
-@AllArgsConstructor(onConstructor_ = @Autowired)
-@ConditionalOnProperty(prefix = "sandbox-data-management", name = "enabled", havingValue = "true")
+@ConditionalOnProperty(prefix = "sandbox-data-management", name = "enabled")
+@Slf4j
 public class SandboxDeleteController {
   private final ObservationRepository observationRepository;
 
   private final QuestionnaireRepository questionnaireRepository;
 
   private final QuestionnaireResponseRepository questionnaireResponseRepository;
+
+  /** Constructor to inject the repositories, and validate the enabled property with booleans. */
+  public SandboxDeleteController(
+      @Value("${sandbox-data-management.enabled}") String isSandboxEnabled,
+      ObservationRepository observationRepository,
+      QuestionnaireRepository questionnaireRepository,
+      QuestionnaireResponseRepository questionnaireResponseRepository) {
+    checkState("true".equals(isSandboxEnabled) || "false".equals(isSandboxEnabled));
+    log.info("Sandbox Data Management Enabled: {}", isSandboxEnabled);
+    this.observationRepository = observationRepository;
+    this.questionnaireRepository = questionnaireRepository;
+    this.questionnaireResponseRepository = questionnaireResponseRepository;
+  }
 
   @DeleteMapping(value = "/Observation/{id}")
   void deleteObservation(@PathVariable("id") String id) {
