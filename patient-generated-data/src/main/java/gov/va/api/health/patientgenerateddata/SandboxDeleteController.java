f81dd24baa2a567075d963da65a1c4822f9a410b
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-04-09 18:26:17 -0400

    .

--- patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/SandboxDeleteController.java
+++ patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/SandboxDeleteController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(
     value = "/sandbox-data/r4",
     produces = {"application/json", "application/fhir+json"})
 @ConditionalOnProperty(prefix = "sandbox-data-management", name = "enabled")
-@Slf4j
 public class SandboxDeleteController {
   private final ObservationRepository observationRepository;
 
@@ -26,14 +26,14 @@ public class SandboxDeleteController {
 
   private final QuestionnaireResponseRepository questionnaireResponseRepository;
 
-  /** Constructor to inject the repositories, and validate the enabled property with booleans. */
-  public SandboxDeleteController(
-      @Value("${sandbox-data-management.enabled}") String isSandboxEnabled,
+  SandboxDeleteController(
+      @Value("${sandbox-data-management.enabled}") String enabled,
       ObservationRepository observationRepository,
       QuestionnaireRepository questionnaireRepository,
       QuestionnaireResponseRepository questionnaireResponseRepository) {
-    checkState("true".equalsIgnoreCase(isSandboxEnabled), "This class is currently disabled as per environment configuration");
-    log.info("Sandbox Data Management Enabled: {}", isSandboxEnabled);
+    checkState(
+        "true".equalsIgnoreCase(enabled), "sandbox-data-management.enabled must be true or false");
+    log.info("Using {}", getClass().getSimpleName());
     this.observationRepository = observationRepository;
     this.questionnaireRepository = questionnaireRepository;
     this.questionnaireResponseRepository = questionnaireResponseRepository;
