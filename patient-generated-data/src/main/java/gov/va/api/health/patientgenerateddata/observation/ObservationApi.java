3012bea5dd704e31164506d1c77d85065655b27e
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-06-22 10:54:48 -0400

    fix structural error in observation swagger

--- patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/observation/ObservationApi.java
+++ patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/observation/ObservationApi.java
@@ -9,16 +9,16 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 
-/** Observation Swagger definition. */
 public interface ObservationApi {
   @Operation(
       summary = "Observation Create",
-      description = "https://www.hl7.org/fhir/r4/observation.html",
+      description = "https://www.hl7.org/fhir/R4/observation.html",
       tags = {"Observation"})
   @POST
   @Path("Observation")
@@ -42,7 +42,7 @@ public interface ObservationApi {
 
   @Operation(
       summary = "Observation Read",
-      description = "https://www.hl7.org/fhir/r4/observation.html",
+      description = "https://www.hl7.org/fhir/R4/observation.html",
       tags = {"Observation"})
   @GET
   @Path("Observation/{id}")
@@ -70,8 +70,43 @@ public interface ObservationApi {
   Observation observationRead(
       @Parameter(
               in = ParameterIn.PATH,
-              name = "_id",
+              name = "id",
               required = true,
+              description =
+                  "The logical ID of the resource. Once assigned, this value never changes.")
+          String id);
+
+  @Operation(
+      summary = "Observation Search",
+      description = "https://www.hl7.org/fhir/R4/observation.html",
+      tags = {"Observation"})
+  @GET
+  @Path("Observation")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              schema = @Schema(implementation = Observation.Bundle.class),
+              mediaType = "application/fhir+json"))
+  @ApiResponse(
+      responseCode = "400",
+      description = "Bad request",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  @ApiResponse(
+      responseCode = "404",
+      description = "Not found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  Observation.Bundle observationSearch(
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "_id",
               description =
                   "The logical ID of the resource. Once assigned, this value never changes.")
           String id,
@@ -79,11 +114,25 @@ public interface ObservationApi {
               in = ParameterIn.QUERY,
               name = "_lastUpdated",
               description = "The date when the record was last updated.")
-          String lastUpdated);
+          String lastUpdated,
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "page",
+              description = "The page number of the search result.")
+          @DefaultValue("1")
+          int page,
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "_count",
+              description =
+                  "The number of resources that should be returned in a single page. "
+                      + "The maximum count size is 100.")
+          @DefaultValue("15")
+          int count);
 
   @Operation(
       summary = "Observation Update",
-      description = "https://www.hl7.org/fhir/r4/observation.html",
+      description = "https://www.hl7.org/fhir/R4/observation.html",
       tags = {"Observation"})
   @PUT
   @Path("Observation/{id}")
@@ -114,7 +163,8 @@ public interface ObservationApi {
               name = "id",
               required = true,
               description =
-                  "The logical ID of the resource." + " Once assigned, this value never changes.")
+                  "The logical ID of the resource. Once assigned, this value never changes.")
           String id,
-      @Parameter(required = true) @RequestBody Observation observation);
+      @RequestBody(required = true, description = "The FHIR resource in JSON format.")
+          Observation body);
 }
