c211bdd25e75c00cff1e848242f61671e307f361
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-08-26 20:02:51 -0400

    .

--- patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/Application.java
+++ patient-generated-data/src/main/java/gov/va/api/health/patientgenerateddata/Application.java
@@ -3,7 +3,6 @@ package gov.va.api.health.patientgenerateddata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** Spring Boot Application. */
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
