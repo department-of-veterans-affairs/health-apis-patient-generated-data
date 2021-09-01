e4da50fe71cac602176a8001576f067f9e947a41
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-09-01 16:43:24 -0400

    first try baby

--- patient-generated-data/src/test/java/gov/va/api/health/patientgenerateddata/SourcererTest.java
+++ patient-generated-data/src/test/java/gov/va/api/health/patientgenerateddata/SourcererTest.java
@@ -3,34 +3,47 @@ package gov.va.api.health.patientgenerateddata;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.nimbusds.jose.JWSObject;
+import java.util.Map;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SourcererTest {
-  // {"foo":"bar"}
-  final String jwtNoCid =
-      "Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.UIZchxQD36xuhacrJF9HQ5SIUxH5HBiv9noESAacsxU";
-
-  // {"cid":"404"}
-  final String jwtUnknown =
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaWQiOiI0MDQifQ.fqKxnjh-31G7uLZ327VQJ6artZi9Q-823sPFowNytck";
-
   Sourcerer s = new Sourcerer("{\"foo\":\"bar\"}", "sat");
 
   @ParameterizedTest
-  @ValueSource(
-      strings = {"foo", "sat", "Bearer", "Bearer ", "Bearer notjson", jwtNoCid, jwtUnknown})
+  @ValueSource(strings = {"foo", "sat", "Bearer", "Bearer ", "Bearer notjson"})
   void badRequest(String auth) {
     assertThrows(Exceptions.BadRequest.class, () -> s.source(auth));
   }
 
   @Test
-  void jwt() {
-    // {"cid":"foo"}
-    String jwtKnown =
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaWQiOiJmb28ifQ.tNSiheCHL-YXNkigpMIGyayWponm6fWsYD6EhBBWwck";
-    assertThat(s.source(jwtKnown)).isEqualTo("https://api.va.gov/services/pgd/bar");
+  @SneakyThrows
+  void jwt_knownClientId() {
+    String jwt =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaWQiOiJmb28ifQ.tNSiheCHL-YXNkigpMIGyayWponm6fWsYD6EhBBWwck";
+    assertThat(JWSObject.parse(jwt).getPayload().toJSONObject()).isEqualTo(Map.of("cid", "foo"));
+    assertThat(s.source("Bearer " + jwt)).isEqualTo("https://api.va.gov/services/pgd/bar");
+  }
+
+  @Test
+  @SneakyThrows
+  void jwt_noClientId() {
+    String jwt =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.UIZchxQD36xuhacrJF9HQ5SIUxH5HBiv9noESAacsxU";
+    assertThat(JWSObject.parse(jwt).getPayload().toJSONObject()).isEqualTo(Map.of("foo", "bar"));
+    assertThrows(Exceptions.BadRequest.class, () -> s.source("Bearer " + jwt));
+  }
+
+  @Test
+  @SneakyThrows
+  void jwt_unknownClientId() {
+    String jwt =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaWQiOiI0MDQifQ.fqKxnjh-31G7uLZ327VQJ6artZi9Q-823sPFowNytck";
+    assertThat(JWSObject.parse(jwt).getPayload().toJSONObject()).isEqualTo(Map.of("cid", "404"));
+    assertThrows(Exceptions.BadRequest.class, () -> s.source("Bearer " + jwt));
   }
 
   @Test
