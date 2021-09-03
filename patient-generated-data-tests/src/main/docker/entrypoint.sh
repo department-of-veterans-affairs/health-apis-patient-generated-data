1acd17746693fa6823903b10660b2d54e65b7ce1
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2021-09-03 09:44:28 -0400

    API-9677 set client-key for ITs (#116)

--- patient-generated-data-tests/src/main/docker/entrypoint.sh
+++ patient-generated-data-tests/src/main/docker/entrypoint.sh
@@ -8,6 +8,7 @@ cd $SENTINEL_BASE_DIR
 test -n "${K8S_ENVIRONMENT}"
 test -n "${K8S_LOAD_BALANCER}"
 
+if [ -z "${CLIENT_KEY:-}" ]; then CLIENT_KEY="$CLIENT_KEY"; fi
 if [ -z "${MAGIC_ACCESS_TOKEN:-}" ]; then MAGIC_ACCESS_TOKEN="$MAGIC_ACCESS_TOKEN"; fi
 if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV="$K8S_ENVIRONMENT"; fi
 if [ -z "${SENTINEL_URL:-}" ]; then SENTINEL_URL="https://${K8S_LOAD_BALANCER}"; fi
@@ -17,6 +18,7 @@ java-tests \
   --regression-test-pattern ".*IT\$" \
   --smoke-test-pattern ".*OpenApiIT\$" \
   -Daccess-token="$MAGIC_ACCESS_TOKEN" \
+  -Dclient-key="$CLIENT_KEY" \
   -Dsentinel="$SENTINEL_ENV" \
   -Dsentinel.internal.url="${SENTINEL_URL}" \
   -Dsentinel.r4.url="${SENTINEL_URL}" \
